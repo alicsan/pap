556a8008fc9a13b5ecb3274834ad2335515c0ec4
MonFlowers <49616301+alsoacat@users.noreply.github.com>
2020-09-22 16:40:04 +0200

    creating db

--- pais/models.py
+++ pais/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Pais(models.Model):
 
-    nombre=models.TextField()
+    nombre=models.CharField(max_length=30)
 
     #especie de get
     def str (self):
